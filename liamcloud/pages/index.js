8fab9fad15db211dc2ed5291ec05fc694cb2a468
Rick Sun <liamcloud@protonmail.com>
2023-02-24 14:59:34 +0100

    socials

--- liamcloud/pages/index.js
+++ liamcloud/pages/index.js
@@ -2,6 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai'
+import { FaTelegramPlane } from 'react-icons/fa'
+import Link from 'next/link'
+import liam from '../public/liam.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,13 +20,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div></div>
+        <div>
+
+        </div>
         <div className='grid'>
           <div>
             <h1>Liam Cloud</h1>
           </div>
         </div>
-        <div>
+        <div className='socials'>
+          <Link href="https://www.twitter.com/imliamcloud" target="_blank" className='socialEl'>
+          <AiOutlineTwitter />
+          </Link>
+          <Link href="https://github.com/liamcloud" target="_blank" className='socialEl'>
+          <AiFillGithub />
+          </Link>
+          <Link href="https://github.com/liamcloud" target="_blank" className='socialEl'>
+          <FaTelegramPlane />
+          </Link>
         </div>
       </main>
     </>
